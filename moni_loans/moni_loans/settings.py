30321808230a5bf80419724086058169fd3c5343
E-Niebylski <esteban.niebylski@gmail.com>
2021-10-01 19:37:11 -0300

    .

--- moni_loans/moni_loans/settings.py
+++ moni_loans/moni_loans/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-deg@&i!2^g^_1!)_@j@ordms$6pj9p0tm!(vu2kf8+xhsidtp1'
+SECRET_KEY = 'DjangoSecretApiKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
